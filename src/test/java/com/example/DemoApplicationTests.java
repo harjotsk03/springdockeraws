70d3caffd241f30a8581b93e7db119e278f77bb3
Harjot Singh <harjotsk03@gmail.com>
2025-09-26 21:53:39 -0700

    Initial commit: Spring Boot base project

---
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test will simply check if the Spring context starts correctly
+    }
+}
